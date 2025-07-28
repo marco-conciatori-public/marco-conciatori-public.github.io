1f7e2e6612b5c3686ece6664b0b78e64ddb743fe
Marco <irotaicnoc@gmail.com>
2025-07-28 17:39:13 +0200

    refactoring

--- gemini/main.js
+++ gemini/main.js
@@ -1,5 +1,3 @@
-// main.js
-
 document.addEventListener('DOMContentLoaded', function() {
     // Function to load reusable HTML content into a placeholder
     async function loadContent(placeholderId, filePath) {
